eee1ddf5dfde3307349eb3767ed7d07243d92126
Manuel Lopez <manuel.lopez@perfectchannel.com>
2017-06-01 18:31:54 +0200

    Added Specflow project with first test

---
+++ SpecflowTests/RetrieveAuctions.feature
@@ -0,0 +1,10 @@
+﻿Feature: Retrieve auctions from API
+	In order to manage auctions in my custom application
+	As an API consumer
+	I want to retrieve distinct auctions from API
+
+@controllerTest
+Scenario: Get all auctions
+	Given A list of 3 auctions stored in database	
+	When I retrieve all actions
+	Then the returned result must have 3 elements
---
+++ SpecflowTests/Steps/AuctionSteps.cs
@@ -0,0 +1,25 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SpecflowTests.Steps
+{
+    public class AuctionSteps
+    {
+        [Given(@"A list of (.*) auctions stored in database")]
+        public void GivenAListOfAuctionsStoredInDatabase(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I retrieve all actions")]
+        public void WhenIRetrieveAllActions()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the returned result must have (.*) elements")]
+        public void ThenTheReturnedResultMustHaveElements(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
